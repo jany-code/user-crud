39e453e153d9bc8d0a2df0900fb04064000258bd
Janyne Ruiz <ruizjanyne@gmail.com>
2025-07-31 13:53:55 -0300

    Settings test

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -20,7 +20,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-RENDER = os.environ.get('RENDER ')
+RENDER = os.environ.get('RENDER')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
